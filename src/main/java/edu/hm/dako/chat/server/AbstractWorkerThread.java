8dffc92864169d921881bbcf3331ab14393e0bb3
Taha Obed <taha.obed@hotmail.de>
2016-11-13 15:01:40 +0100

    [Methoden] - In verschiedenen Klassen Methoden hinzugefügt

--- src/main/java/edu/hm/dako/chat/server/AbstractWorkerThread.java
+++ src/main/java/edu/hm/dako/chat/server/AbstractWorkerThread.java
@@ -51,49 +51,39 @@ public abstract class AbstractWorkerThread extends Thread {
 
 	/**
 	 * Senden eines Login-List-Update-Event an alle angemeldeten Clients
-	 * 
-	 * @param pdu
-	 *          Zu sendende PDU
+	 * @param pdu Zu sendende PDU
 	 */
 	protected abstract void sendLoginListUpdateEvent(ChatPDU pdu);
 
 	/**
 	 * Aktion fuer die Behandlung ankommender Login-Requests: Neuen Client anlegen
 	 * und alle Clients informieren
-	 * 
-	 * @param receivedPdu
-	 *          Empfangene PDU
+	 * @param receivedPdu Empfangene PDU
 	 */
 	protected abstract void loginRequestAction(ChatPDU receivedPdu);
 
 	/**
 	 * Aktion fuer die Behandlung ankommender Logout-Requests: Alle Clients
 	 * informieren, Response senden und Client loeschen
-	 * 
-	 * @param receivedPdu
-	 *          Empfangene PDU
+	 * @param receivedPdu Empfangene PDU
 	 */
 	protected abstract void logoutRequestAction(ChatPDU receivedPdu);
 
 	/**
 	 * Aktion fuer die Behandlung ankommender ChatMessage-Requests: Chat-Nachricht
 	 * an alle Clients weitermelden
-	 * 
-	 * @param receivedPdu
-	 *          Empfangene PDU
+	 * @param receivedPdu Empfangene PDU
 	 */
 	protected abstract void chatMessageRequestAction(ChatPDU receivedPdu);
 
 	/**
 	 * Aktion fuer die Behandlung ankommender ChatMessageConfirm-PDUs
-	 * 
 	 * @param receivedPdu
 	 */
 
 	/**
 	 * Verarbeitung einer ankommenden Nachricht eines Clients (Implementierung des
 	 * serverseitigen Chat-Zustandsautomaten)
-	 * 
 	 * @throws Exception
 	 */
 	protected abstract void handleIncomingMessage() throws Exception;
